5560bdd923759a9a159b45cb512f6021333c6d9d
Kirbit04 <145982858+Kirbit04@users.noreply.github.com>
2025-02-28 13:40:58 +0300

    Finished the acquisition pages. Created both buyer and tenants forms with a bit of backend.

--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f76518693eded050c0c9ce8ae6e320f8ba75df08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+674b31f9edb5de941722a7ad9c1b63a81e53aae2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
